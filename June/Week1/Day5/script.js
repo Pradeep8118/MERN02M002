1b22fbb350930db276097c36e256b6a9a3dd3ad3
Pradeep8118 <pp8788022@gmail.com>
2025-06-07 10:30:52 +0530

    loginpage Saved

---
+++ June/Week1/Day5/script.js
@@ -0,0 +1,169 @@
+// console.log("Welcome")
+// let a = 1
+// let A=1
+// console.log(a)
+// console.log(A)
+// let _ = "Qlith";
+// console.log(_)
+// let $ ="Qlith Innovation";
+// console.log($)
+
+// let $name="xyz"
+// console.log($name)
+// let _name="y"
+// console.log(_name)
+
+// let nameOfStudent="Bibek";
+// console.log(nameOfStudent)
+
+// let a = 20;
+// console.log(a)
+// console.log(typeof a)
+
+// let name="Qlith";
+// console.log(name ,typeof name)
+
+// let isPass = true;
+// console.log(isPass)
+// console.log(typeof isPass)
+
+// let empty=undefined;
+// console.log(empty)
+// console.log(typeof empty)
+
+// let nullValue=null;
+// console.log(nullValue)
+// console.log(typeof nullValue)
+
+// let c=30;
+// console.log(c);
+// console.log(typeof typeof c)
+
+// let b;
+// console.log(b);
+
+
+// let d=30.5;
+// console.log(typeof d);
+// //Golbal declration of the variable
+// var fullName="Qlith Innovation";
+// console.log(fullName)
+// {
+// console.log(fullName)
+// }
+// function getName(){
+//     console.log(fullName)
+// }
+// getName()
+
+// //normal block
+// {
+//     var a=40;
+//     console.log("block level declaration of variable",a)
+// }
+// console.log( "local access",a)
+
+// function getA(){
+//     console.log("function level declaration")
+// }
+// getA();
+
+// function variableDeclare(){
+//     var r=50;
+//     console.log(r)
+// }
+// variableDeclare()
+// console.log(r)
+
+
+//This is the let variable declaration
+
+// let name="Innovation";
+// console.log(name);
+// {
+//     console.log(name)
+// }
+
+// function getName(){
+//     console.log(name)
+// }
+// getName()
+
+// //block level declration
+// {
+//     let a=30;
+//     console.log(a)
+// }
+// console.log("local access:",a)
+
+// function getA(){
+//     console.log("function level declaration",a)
+// }
+// getA()
+
+//This is the const variable declaration
+// const name="xyz"
+// console.log(name)
+// {
+//     console.log(name)
+// }
+
+// function getName(){
+//     console.log(name)
+// }
+
+// {
+//     const a=70;
+//     console.log(a);
+// }
+
+// console.log(a)
+
+// var a=60;
+// console.log(a)
+
+// // a =40;
+// // console.log(a)
+
+// // a="y"
+// // console.log(a)
+
+// var a=30;
+// console.log(a)
+
+// let a=50;
+// console.log(a)
+// a=80;
+// console.log(a)
+
+// const s="String";
+// console.log(s)
+// s="Rohan";
+// console.log(s)
+
+// const s="sdfay";
+// console.log(first)
+
+//Difference between var,let,const
+// redeclaration :
+// var => possible
+// let =>not possible
+// const => not possible
+
+// ReInitialization:
+// var => possible
+// let => possible
+// const => not possible
+
+//Only Delclaration possi
+
+
+
+
+
+
+// let a;
+// console.log(a)
+
+// const b;
+// console.log(a)
